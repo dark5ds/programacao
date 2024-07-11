658f3d37e5c802f9df74a03491f7ed0e54c01553
Daniel Silva <dani.silva259@gmail.com>
2024-07-11 15:55:27 +0100

    Nova sessão de programação / aprender de novo!

---
+++ Python/primeira aula/main.py
@@ -0,0 +1,55 @@
+print("Hello, World! My name is Dark5ds")
+#comentário
+
+a = "Python"
+b = "is"
+c = "Awesome"
+
+print(a, b, c)
+print(a  + b  + c)
+
+d = 1
+e = 2.8
+f = "1op"
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+print(d+e)
+
+x=15E4
+
+print(type(x))
+
+
+a1 = int(1)
+a2 = int(2.7)
+a3 = int(3)
+
+print(a1 , a2 , a3)
+
+#array
+
+z= "Hello world"
+print(z[0])
+
+#listas
+
+lista = ["Banana" , "Mançã" , "Chocolate"]
+print(lista)
+
+print(lista[2])
+
+lista[0] = "pera"
+
+#cilcos
+print(lista)
+for x in lista:
+    print(x)
+
+
+
+
+
+
